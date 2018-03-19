44e1e58adaf51abca01b780b6860d0fad866d4f8
DanielHowe <dhowe@csumb.edu>
2018-03-19 01:05:23 -0700

    Warm up and problem 1: First commit
    Added a few functions to play around with the lab problems

---
+++ Lab 7 - Group B/lab7_howe_d.py
@@ -0,0 +1,71 @@
+# Lab 7
+# Pelican Collective Sub-Group: Daniel, Rogelio, Jiwan
+
+def snowMan(pic):
+  #filename = pickAFile()
+  #pic = makePicture(filename)
+  #explore(pic)
+  xCordinate = requestInteger("Where should the Snowman go? Please enter X coordinate:")
+  yCordinate = requestInteger("Where should the Snowman go? Please enter Y coordinate:")
+  snowBallSize = requestInteger("How big should the base of the Snowman be?")
+  while (xCordinate - snowBallSize / 2 <= 0 or getWidth(pic) < xCordinate + snowBallSize):
+    xCordinate = requestInteger("The Snowman is too wide for the coordinates provided. Please enter new X coordinate:")
+  offset = 0
+  for i in range(0,3):
+    if (i == 2):
+      # The nose is drawn fist so that it is below the oval that will be drawn below (the end of the for loop)
+      print "Drawing nose.."
+      startX = int(xCordinate + offset / 2)
+      startY = int(yCordinate - offset + snowBallSize / 2)
+      endX = int(startX + (snowBallSize / 2) + (snowBallSize * 0.5))
+      endY = startY - 2
+      print "First line of the nose starts at %d, %d, and ends at %d, %d" % (startX, startY, endX, endY)
+      addLine(pic, startX, startY, endX, endY, red)
+      startX = startX - 10
+      print "Second line of the nose starts at %d, %d, and ends at %d, %d" % (startX, startY, endX, endY)
+      addLine(pic, startX, startY, endX, endY, red)      
+    print "Drawing %d oval at %d, %d with the size of %d by %d" % (i, int(xCordinate - offset / 2), int(yCordinate + offset), int(snowBallSize), int(snowBallSize * 0.95))
+    addOvalFilled(pic, int(xCordinate + offset / 4), int(yCordinate - offset), int(snowBallSize), int(snowBallSize * 0.95), white)
+    if (i == 2):
+      print "Drawing eye"
+      separation = int(snowBallSize / 6)
+      sizeEye = int(snowBallSize / 7)
+      startX = int(xCordinate + offset / 2 + separation)
+      startY = int(yCordinate - offset + snowBallSize / 2 - sizeEye)
+      addOvalFilled(pic, startX, startY, sizeEye, sizeEye, black)
+      print "Drawing eye at %d, %d and is %d big" % (startX, startY, sizeEye)
+      startX = int(xCordinate + offset / 2 - separation)
+    if (i == 1):
+      print "Drawing arm"
+      startX = int(xCordinate + offset)
+      startY = int(yCordinate - offset + snowBallSize / 2)
+      endX = int(xCordinate)
+      endY = int(yCordinate - offset / 2)
+      print "Line of the arm starts at %d, %d, and ends at %d, %d" % (startX, startY, endX, endY)
+      addLine(pic, startX, startY, endX, endY, black)
+    offset = offset + (snowBallSize * 0.5)
+    snowBallSize = snowBallSize * 0.75
+  return pic
+  
+def insertText(pic):
+  #filename = pickAFile()
+  #pic = makePicture(filename)
+  xCordinate = requestInteger("Where should the text go? Please enter X coordinate:")
+  yCordinate = requestInteger("Where should the text go? Please enter Y coordinate:")
+  text = requestString("Enter the text:")
+  while (getWidth(pic) < xCordinate or xCordinate < 0):
+    xCordinate = requestInteger("The text won't show in that coordinate.. Please enter new X coordinate:")
+  while (getHeight(pic) < yCordinate or yCordinate < 0):
+    yCordinate = requestInteger("The text won't show in that coordinate.. Please enter Y coordinate:")
+  print "Adding text to image.."
+  print "Length is %d" % (len(text))
+  addText(pic, xCordinate, yCordinate, text)
+  return pic
+  
+def talkingSnowMan():
+  filename = pickAFile()
+  pic = makePicture(filename)
+  pic = snowMan(pic)
+  #show(pic)
+  pic = insertText(pic)
+  show(pic)
