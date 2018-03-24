cd75007e7a16228f6be62ff9a04e02c4368a08a6
DanielHowe <dhowe@csumb.edu>
2018-03-23 22:05:38 -0700

    updated program..dude

--- midterm - Group B/midterm_howe_d.py
+++ midterm - Group B/midterm_howe_d.py
@@ -4,7 +4,63 @@
 
 import random
 import time
+# Make the numbers random
 random.seed(time.time())
+
+def lineDrawing(pic, threshold):
+  print 'Converting image to line drawing with a threshold of %d' % (threshold,)
+  # Begin searching image for pixels that match the condition: 
+  # If there is a large enough difference to find the difference, 
+  # subtract and use the abs function to get the absolute value in the 
+  # luminance between your pixel and BOTH the pixels to the right and below,
+  # then make your pixel black.  Otherwise, make your pixel white
+  for x in range(0, getWidth(pic)):
+    for y in range(0, getHeight(pic)):
+      # Get the current pixel and store colors
+      px = getPixel(pic, x, y)
+      red = getRed(px)
+      green = getGreen(px)
+      blue = getBlue(px)
+      currentLuminosity = red * 0.299 + blue * 0.114 + green * 0.587
+      if (x + 1 < getWidth(pic)):
+        # Get the pixel to the right and store colors
+        px = getPixel(pic, x + 1, y)
+        red = getRed(px)
+        green = getGreen(px)
+        blue = getBlue(px)
+        leftLuminosity = red * 0.299 + blue * 0.114 + green * 0.587
+        if (y + 1 < getHeight(pic)):
+          # Get the pixel below and store colors 
+          px = getPixel(pic, x, y + 1)
+          red = getRed(px)
+          green = getGreen(px)
+          blue = getBlue(px)
+          bottomLuminosity = red * 0.299 + blue * 0.114 + green * 0.587
+          if (abs(bottomLuminosity - currentLuminosity) > threshold and abs(leftLuminosity - currentLuminosity) > threshold):
+            # Get current x,y pixel
+            px = getPixel(pic, x, y)
+            # Set pixel to black
+            setRed(px, 0)
+            setGreen(px, 0)
+            setBlue(px, 0)
+      else:
+        # Either the for loop has reached the edge of the image or non of the conditions were matched above
+        # Set the pixel to white
+        px = getPixel(pic, x, y)
+        setRed(px, 255)
+        setGreen(px, 255)
+        setBlue(px, 255)
+  return(pic)
+      
+def betterBnW(pic):
+  print 'Converting image to black and white'
+  pixels = getPixels(pic)
+  for i in pixels:
+    luminosity = ((getRed(i)*0.299) + (getBlue(i)*0.114) + (getGreen(i)*0.587))
+    setRed(i, luminosity)
+    setBlue(i, luminosity)
+    setGreen(i, luminosity)
+  return(pic)
 
 def artify(sourceImage):
   for x in range(0, getWidth(sourceImage)):
@@ -14,29 +70,30 @@ def artify(sourceImage):
       green = getGreen(px)
       blue = getBlue(px)
       ran = random.random()
+      ran = ran + 1
       if red < 64:
         # Use modifier in the second argument below
-        setRed(px, 31 + ran)
+        setRed(px, 31 * ran)
       elif red > 63 and red < 128:
-        setRed(px, 95 + ran)
+        setRed(px, 95 * ran)
       elif red > 127 and red < 192:
-        setRed(px, 159 + ran)
+        setRed(px, 159 * ran)
       else: # 191 < color < 256
         setRed(px, 223)
       
       if green < 64:
-        setGreen(px, 31 + ran)
+        setGreen(px, 31 * ran)
       elif green > 63 and green < 128:
-        setGreen(px, 95 + ran)
+        setGreen(px, 95 * ran)
       elif green > 127 and green < 192:
-        setGreen(px, 109 + ran)
+        setGreen(px, 109 * ran)
       else: # 191 < color < 256
         setBlue(px, 223)
       
       if blue < 64:
-        setBlue(px, 31 + ran)
+        setBlue(px, 31 * ran)
       elif blue > 63 and blue < 128:
-        setBlue(px, 95 + ran)
+        setBlue(px, 95 * ran)
       elif blue > 127 and blue < 192:
         setBlue(px, 159)
       else: # 191 < color < 256
@@ -44,123 +101,6 @@ def artify(sourceImage):
   #show(pic)
   #writePictureTo(pic, filename + "_Artified.jpg")
   return(sourceImage)
-
-def artify1(sourceImage):
-  for x in range(0, getWidth(sourceImage)):
-    for y in range(0, getHeight(sourceImage)):
-      px = getPixel(sourceImage, x, y)
-      red = getRed(px)
-      green = getGreen(px)
-      blue = getBlue(px)
-      ran = random.random()
-      if red < 64:
-        # Use modifier in the second argument below
-        setRed(px, 31 + ran)
-      elif red > 63 and red < 128:
-        setRed(px, 95 + ran)
-      elif red > 127 and red < 192:
-        setRed(px, 109 + ran)
-      else: # 191 < color < 256
-        setRed(px, 223 + ran)
-      
-      if green < 64:
-        setGreen(px, 31 + ran)
-      elif green > 63 and green < 128:
-        setGreen(px, 95 + ran)
-      elif green > 127 and green < 192:
-        setGreen(px, 159 + ran)
-      else: # 191 < color < 256
-        setBlue(px, 223 + ran)
-      
-      if blue < 64:
-        setBlue(px, 31 + ran)
-      elif blue > 63 and blue < 128:
-        setBlue(px, 95 + ran)
-      elif blue > 127 and blue < 192:
-        setBlue(px, 159)
-      else: # 191 < color < 256
-        setBlue(px, 223 + ran)
-  #show(pic)
-  #writePictureTo(pic, filename + "_Artified.jpg")
-  return(sourceImage)
-
-def artify2(sourceImage):
-  for x in range(0, getWidth(sourceImage)):
-    for y in range(0, getHeight(sourceImage)):
-      px = getPixel(sourceImage, x, y)
-      red = getRed(px)
-      green = getGreen(px)
-      blue = getBlue(px)
-      ran = random.random()
-      if red < 64:
-        # Use modifier in the second argument below
-        setRed(px, 31 + ran)
-      elif red > 63 and red < 128:
-        setRed(px, 95 + ran)
-      elif red > 127 and red < 192:
-        setRed(px, 159 + ran)
-      else: # 191 < color < 256
-        setRed(px, 223 + ran)
-      
-      if green < 64:
-        setGreen(px, 31 + ran)
-      elif green > 63 and green < 128:
-        setGreen(px, 95 + ran)
-      elif green > 127 and green < 192:
-        setGreen(px, 159 * ran)
-      else: # 191 < color < 256
-        setBlue(px, 223 + ran)
-      
-      if blue < 64:
-        setBlue(px, 31 + ran)
-      elif blue > 63 and blue < 128:
-        setBlue(px, 95 + ran)
-      elif blue > 127 and blue < 192:
-        setBlue(px, 159)
-      else: # 191 < color < 256
-        setBlue(px, 223 + ran)
-  #show(pic)
-  #writePictureTo(pic, filename + "_Artified.jpg")
-  return(sourceImage)
-def artify3(sourceImage):
-  for x in range(0, getWidth(sourceImage)):
-    for y in range(0, getHeight(sourceImage)):
-      px = getPixel(sourceImage, x, y)
-      red = getRed(px)
-      green = getGreen(px)
-      blue = getBlue(px)
-      ran = random.random()
-      if red < 64:
-        # Use modifier in the second argument below
-        setRed(px, 31 + ran)
-      elif red > 63 and red < 128:
-        setRed(px, 95 + ran)
-      elif red > 127 and red < 192:
-        setRed(px, 159 + ran)
-      else: # 191 < color < 256
-        setRed(px, 223 + ran)
-      
-      if green < 64:
-        setGreen(px, 31 + ran)
-      elif green > 63 and green < 128:
-        setGreen(px, 95 + ran)
-      elif green > 127 and green < 192:
-        setGreen(px, 159 * ran)
-      else: # 191 < color < 256
-        setBlue(px, 223 + ran)
-      
-      if blue < 64:
-        setBlue(px, 31 + ran)
-      elif blue > 63 and blue < 128:
-        setBlue(px, 95 + ran)
-      elif blue > 127 and blue < 192:
-        setBlue(px, 159)
-      else: # 191 < color < 256
-        setBlue(px, 223 + ran)
-  #show(pic)
-  #writePictureTo(pic, filename + "_Artified.jpg")
-  return(sourceImage)
-
 
 def pyCopy(source, target, targetX, targetY):
   print 'Starting copy of '
@@ -191,50 +131,35 @@ def makeWarhol(sourceImage):
   #Store x and y values for the start of the image to track where the next image needs to be placed
   lastX = 0
   lastY = 0
-  for i in range(1, 5):
+  for i in range(1, 3):
     if i == 1:
       print 'Working on picture: %d' % (i,)
-
-      #randomModifier = random.uniform(0.1, 0.9)
-      #pic = artify(sourceImage, randomModifier)
-      pic = artify(sourceImage)
-
-      newImage = pyCopy(pic, newImage, lastX, lastY)
-      lastX = getWidth(pic) + lastX
+      pic1 = artify(sourceImage)
+      newImage = pyCopy(pic1, newImage, lastX, lastY)
+      lastX = getWidth(pic1) + lastX
     elif i == 2:
       print 'Working on picture: %d' % (i,)
-
-      #randomModifier = random.uniform(0.1, 0.9)
-      #pic = artify(sourceImage, randomModifier)
-      pic = artify1(sourceImage)
-
-      newImage = pyCopy(pic, newImage, lastX, lastY)
+      pic2 = artify(sourceImage)
+      newImage = pyCopy(pic2, newImage, lastX, lastY)
       lastX = 0
-      lastY = getHeight(pic)
+      lastY = getHeight(pic2)
     elif i == 3:
       print 'Working on picture: %d' % (i,)
-
-      #randomModifier = random.uniform(0.1, 0.9)
-      #pic = artify(sourceImage, randomModifier)
-      pic = artify2(sourceImage)
-
-      newImage = pyCopy(pic, newImage, lastX, lastY)
-      lastX = getWidth(pic) + lastX
+      pic3 = artify(sourceImage)
+      newImage = pyCopy(pic3, newImage, lastX, lastY)
+      lastX = getWidth(pic3) + lastX
     elif i == 4:
       print 'Working on picture: %d' % (i,)
-
-      #randomModifier = random.uniform(0.1, 0.9)
-      #pic = artify(sourceImage, randomModifier)
-      pic = artify3(sourceImage)
-      newImage = pyCopy(pic, newImage, lastX, lastY)
+      pic4 = artify(sourceImage)
+      newImage = pyCopy(pic4, newImage, lastX, lastY)
   print 'Completed Warhol collage will now return image..' 
-  #repaint(newImage)
-  #writePictureTo(newImage, "/Users/danielhowe/Desktop/collageOutput.jpg")
   return newImage
 
 def test():
   print 'run this function to test the makeWarhol implementation'
   filename = pickAFile()
   image = makePicture(filename)
-  image = makeWarhol(image)
-  writePictureTo(image, pickAFile())
+  image = betterBnW(image)
+  image = lineDrawing(image, 0.9)
+  newImage = makeWarhol(image)
+  writePictureTo(newImage, pickAFile())
