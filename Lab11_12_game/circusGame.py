8f332a59cbca1582051a810be17355a5cffae697
rockycsumb <romoreno@csumb.edu>
2018-03-30 15:37:59 -0700

    Added execMovement
    added execMovement Function

--- Lab11_12_game/circusGame.py
+++ Lab11_12_game/circusGame.py
@@ -65,3 +65,61 @@ def dispHelp(): #Nikola Petkov
   print "                     northwest, northeast, southeast, southwest."
   print "ex. \"go east\" will move you east of current location (if it exists)"
   print "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
+  
+
+def execMovement(location, command):  #Rocky Moreno
+  ticketGate = 'ticket gate'
+  circusTent = 'circus tent'
+  spectatorSeats = 'spectator seats'
+  stagingArea = 'staging area'
+  backStage = 'back stage'
+  newLocation = ''
+  
+  #Location is Ticket Gate
+  if (location == ticketGate): 
+    if(command == 'n' or command == 'north'):
+      newLocation = circusTent
+      
+  #Location is Circus Tent    
+  elif (location == circusTent):
+    if(command == 's' or command == 'south'):
+      newLocation = ticketGate
+    elif(command == 'n' or command == 'north'):
+      newLocation = backStage
+    elif(command == 'w' or command == 'west'):
+      newLocation = spectatorSeats
+    elif(command == 'e' or command == 'east'):
+      newLocation = stagingArea
+      
+  #Location is Specator Seats
+  elif (location == spectatorSeats):
+    if(command == 'se' or command == 'southeast'):
+      newLocation = ticketGate
+    elif(command == 'e' or command == 'east'):
+      newLocation = circusTent
+  
+  #Location is Staging Area
+  elif (location == stagingArea):
+    if(command == 'w' or command == 'west'):
+      newLocation = circusTent
+    elif(command == 'nw' or command == 'northwest'):
+      newLocation = backStage
+      
+  #Location is Back Stage
+  elif (location == backStage):
+    if(command == 's' or command == 'south'):
+      newLocation = circusTent
+    elif(command == 'se' or command == 'southeast'):
+      newLocation = stagingArea
+      
+  #if not valid direction print cant go thisway
+  #else
+  #display and return newLocation
+  #dispLocation(newLocation) 
+  if (newLocation == ''):
+    print 'can\'t go this way'
+    dispLocation(location)                  
+    return location
+  else:
+    dispLocation(newLocation)              
+    return newLocation
