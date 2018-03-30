b2ed1f186ce94d82955a95064a94ae3f638c44fd
jatse <jatse@csumb.edu>
2018-03-29 22:45:48 -0700

    de-camel-cased isAlpha()

--- Lab11_12_game/circusGame.py
+++ Lab11_12_game/circusGame.py
@@ -22,10 +22,9 @@ def main(): #Jason Tse
     #prompt player for command
     command = requestString("What will you do?")
     command = command.lower().strip()
-    while len(command) < 1 or not command.isAlpha():
+    while len(command) < 1 or not command.isalpha():
       command = requestString("I didn't understand. What will you do?")
       command = command.lower().strip()
-      
     
     #parse input into command
     command, comTypeCode = parseCommand(command, comTypeCode)
