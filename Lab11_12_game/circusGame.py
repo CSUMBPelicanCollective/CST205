d1fcb72f5689f2ac6726370eff194d723b4a1e78
jatse <jatse@csumb.edu>
2018-03-29 21:11:47 -0700

    typo fixes

--- Lab11_12_game/circusGame.py
+++ Lab11_12_game/circusGame.py
@@ -15,14 +15,14 @@ def main(): #Jason Tse
   print "abandoned circus. In the wind you could hear faint giggling."
   print "Type help for more information."
   print "Type exit to give up.\n"
-  #dispLocation(location)
+  dispLocation(location)
 	
   #main game loop
   while True:
     #prompt player for command
     command = requestString("What will you do?")
     command = command.lower().strip()
-    while len(command) < 1 or not len.isAlpha():
+    while len(command) < 1 or not command.isAlpha():
       command = requestString("I didn't understand. What will you do?")
       command = command.lower().strip()
       
