cbfc509332ce47566c307a26b45fa0565e33e747
jatse <jatse@csumb.edu>
2018-03-29 21:02:26 -0700

    added first half of main function

--- Lab11_12_game/circusGame.py
+++ Lab11_12_game/circusGame.py
@@ -1,3 +1,31 @@
 #PELICAN COLLECTIVE
 #LAB 11 AND LAB 12
 #GAME
+
+def main(): #Jason Tse
+  #initialize game variables
+  location = "ticket gate"
+  command = "none"
+  comTypeCode = 99
+	
+  #initStory: start user UI
+  print "You suddenly awoke on the hard ground outdoors."
+  print "As your eyes adjust to the dimly moon-lit surroundings,"
+  print "you find yourself inside the front gates of an old"
+  print "abandoned circus. In the wind you could hear faint giggling."
+  print "Type help for more information."
+  print "Type exit to give up.\n"
+  #dispLocation(location)
+	
+  #main game loop
+  while True:
+    #prompt player for command
+    command = requestString("What will you do?")
+    command = command.lower().strip()
+    while len(command) < 1 or not len.isAlpha():
+      command = requestString("I didn't understand. What will you do?")
+      command = command.lower().strip()
+      
+    
+    #parse input into command
+    command, comTypeCode = parseCommand(command, comTypeCode)
