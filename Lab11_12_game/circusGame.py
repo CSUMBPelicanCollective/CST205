7d8ecfe1d3958266269c6e5bc6ab6a0a2da148cf
DanielHowe <dhowe@csumb.edu>
2018-03-31 18:47:22 -0700

    Merged parseCommand into master

--- Lab11_12_game/circusGame.py
+++ Lab11_12_game/circusGame.py
@@ -3,99 +3,57 @@
 #GAME
 
 def main(): #Jason Tse
-  # initStory
-  # initialize game variables
+  #initialize game variables
   location = "ticket gate"
   command = "none"
   comTypeCode = 99
-  	
+	
+  #initStory: start user UI
+  print "You suddenly awoke on the hard ground outdoors."
+  print "As your eyes adjust to the dimly moon-lit surroundings,"
+  print "you find yourself inside the front gates of an old"
+  print "abandoned circus. In the wind you could hear faint giggling."
+  print "Type help for more information."
+  print "Type exit to give up.\n"
+  dispLocation(location)
+	
   #main game loop
-  while (comTypeCode > 0):
-    dispLocation(location, comTypeCode)
-    location, comTypeCode = parseCommand(requestString("What will you do?"), location, comTypeCode)
-
-        
-#####################
-# Display and Print #
-#####################
-
-def parseCommand(userCommand, userLocation, isCode):
-  userCommand = userCommand.lower()
-  newLocation = ''
-  newTempStatus = ''
-  if (userCommand == 'exit'):
-    # Exit
-    isCode = 0
-  elif (userCommand == 'help'):
-    # Print Help
-    isCode = 3
-    dispHelp()
-    response = requestString("Press any key to continue")
-    printPadding()
-  elif (userCommand == 'go'):   
-    # Move
-    isCode = 2
-    where = requestString("Where do you want to GO ?\nExamples: north,south,east,west")
-    acceptableDirections = "north,south,east,west"
-    # Not sure if up, down, left, and right are acceptable
-    # print 'should we execute movement now?\n'
-    # print containsString(where, acceptableDirections)
-    if (containsString(where, acceptableDirections)):
-      lewLocation = execMovement(userLocation, where)
-      return newLocation, isCode
-    printPadding()
+  while True:
+    #prompt player for command
+    command = requestString("What will you do?")
+    command = command.lower().strip()
+    while len(command) < 1 or not command.isalpha():
+      command = requestString("I didn't understand. What will you do?")
+      command = command.lower().strip()
     
-  elif (userCommand == 'look'):
-    isCode = 1
-    where = requestString("Where do you want to LOOK ?\nExamples: here,north,south,east,west")
-    acceptableDirections = "here,north,south,east,west"
-    # Not sure if up, down, left, and right are acceptable
-    if (containsString(where, acceptableDirections)):
-      # Assigning new location to check ahead
-      newTempStatus = execMovement(newLocation, where)
-      dispLocation(newTempStatus, isCode)
-      printPadding()
-      return userLocation, isCode
-      
-  else:
-    dispLocation(userLocation, isCode)
-    printPadding()
-  return userLocation, isCode
-
-def containsString(searchTerm, searchString):
-  if (searchString.find(searchTerm) > 0):
-    return true
-  return false
-
-def printPadding():
-  print '\n\n'
-        
-def dispLocation(location, isCode): #Nikola Petkov
-  if isCode == 99:
-    print "You suddenly awoke on the hard ground outdoors."
-    print "As your eyes adjust to the dimly moon-lit surroundings,"
-    print "you find yourself inside the front gates of an old"
-    print "abandoned circus. In the wind you could hear faint giggling.\n"
-    print "Type help for more information."
-    print "Type exit to give up."
-    printPadding()
- 
-  print "You are currently at the " + str(location)
+    #parse input into command
+    command, comTypeCode = parseCommand(command, comTypeCode)
+  
+    if (comTypeCode == 0):#Jiwanjot Sandhu
+      #break (checking the behaviour of break statement)
+      print "End Game"
+      break
+    elif (comTypeCode == 1):
+      dispLocation(location)
+    elif (comTypeCode == 2):
+      execMovement(location, command) 
+    elif (comTypeCode ==3):
+      dispHelp()
+    elif (comTypeCode == 4):
+      yell(location)
+    
+def dispLocation(location): #Nikola Petkov
+  print "You are currently at the " + location
   if location.lower() == "ticket gate":
-    print "Some description"
-    printPadding()
+    print "The turnstyles are locked and looks difficult to climb over.\n To the NORTH you see a big tent. The tent has smaller entrance NORTHEAST.\n Through the gate bars you spot a clown in the distance. Maybe you can YELL out to them."
   elif location.lower() == "circus tent":
-    print "Some description"
-    printPadding()
+    print "The insides are strung with dim holiday lights. Doesn't look like there have been any shows recently.\n The spectator seats are to the EAST. There is an opening in the tent NORTH, SOUTH, and WEST."
   elif location.lower() == "spectator seats":
-    print "Some description"
-    printPadding()
+    print "The seats are littered with old tickets, discarded cups, and candy wrappers.\n From the seats you can see the all of the main stage to the EAST and an exit to the SOUTHEAST."
   elif location.lower() == "staging area":
-    print "Some description"
-    printPadding()
+    print "The equipment for the shows are kept here, including empty animal cages.\n There are entrances to the WEST and NORTHWEST."
   elif location.lower() == "backstage":
-    print "Some description"
-    printPadding()
+    print "It's a small backstage area. The counters along the outside are cluttered with junk.\n There are entrances to the SOUTH and SOUTHEAST."
 
 def dispHelp(): #Nikola Petkov
   print "- - - - - - - - - - - - - H E L P - - - - - - - - - - - - -"
@@ -107,9 +65,6 @@ def dispHelp(): #Nikola Petkov
   print "ex. \"go east\" will move you east of current location (if it exists)"
   print "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
   
-#############
-# Modifiers #
-#############
 
 def execMovement(location, command):  #Rocky Moreno
   ticketGate = 'ticket gate'
@@ -117,7 +72,7 @@ def execMovement(location, command):  #Rocky Moreno
   spectatorSeats = 'spectator seats'
   stagingArea = 'staging area'
   backStage = 'backstage'
-  newLocation = ''  
+  newLocation = ''
   
   #Location is Ticket Gate
   if (location == ticketGate): 
@@ -125,7 +80,7 @@ def execMovement(location, command):  #Rocky Moreno
       newLocation = circusTent
     elif(command == 'nw' or command == 'northwest'):
       newLocation = spectatorSeats
-      
+
   #Location is Circus Tent    
   elif (location == circusTent):
     if(command == 's' or command == 'south'):
@@ -159,9 +114,122 @@ def execMovement(location, command):  #Rocky Moreno
       newLocation = stagingArea
       
   #if not valid direction print cant go thisway
-  elif (newLocation == ''):
-    print 'You CANNOT go this way!\n\n'
-    #dispLocation(location)                  
+  #else
+  #display and return newLocation
+  #dispLocation(newLocation) 
+  if (newLocation == ''):
+    print 'can\'t go this way'                 
     return location
   else:
-    return location
+    dispLocation(newLocation)              
+    return newLocation
+
+#For yelling action. Only useful at ticket gate.        
+def yell(location): #Jason Tse
+  if location == "ticket gate":
+    print "The clown seems to ignore you."
+  else:
+    print "Now is not the time for that."
+
+def parseCommand(userCommand, isCode):
+  # Check user commands for matching program status code
+  extraVerb, testGo = commandCheck("go", userCommand, 2)
+  
+  
+  if (userCommand == "exit"):
+    # Exit
+    isCode = 0
+    print 'exit'
+    return userCommand, isCode
+  elif (userCommand == "help"):
+    # Print Help
+    isCode = 3
+    print 'help'
+    return userCommand, isCode
+  elif (userCommand == "look"):
+    isCode = 1
+    print 'look'
+    return userCommand, isCode
+  elif (testGo):  
+    # Move / Go
+    # Initialization of variables
+    newCommand = userCommand.strip('go')
+    isCode = 66
+    if (len(newCommand) < 4):
+      return newCommand, isCode
+    
+    goodDirection = false
+    acceptableDirections = "east,west"
+    wordLength = 4
+    newCommand4 = acceptableDirections
+    newCommand4, goodDirection = commandCheck(newCommand, acceptableDirections, wordLength)
+    if (goodDirection == true):
+      isCode = 2
+      return newCommand4, isCode
+      
+    goodDirection = false
+    acceptableDirections = "east"
+    wordLength = 4
+    newCommand = acceptableDirections
+    newCommand, goodDirection = commandCheck(newCommand, acceptableDirections, wordLength)
+    if (goodDirection == true):
+      isCode = 2
+      return acceptableDirections, isCode
+    
+    goodDirection = false
+    acceptableDirections = "southeast,northeast,southwest,northwest"
+    wordLength = 9
+    newCommand8 = acceptableDirections
+    newCommand8, goodDirection = commandCheck(newCommand, acceptableDirections, wordLength)
+    if (goodDirection == true):
+      isCode = 2
+      return newCommand8, isCode
+    
+    goodDirection = false
+    acceptableDirections = "north,south"
+    wordLength = 6
+    newCommand5 = acceptableDirections
+    newCommand5, goodDirection = commandCheck(newCommand, acceptableDirections, wordLength)
+    if (goodDirection == true):
+      isCode = 2
+      return newCommand5, isCode
+      
+  print 'returning default values'
+  return "",99
+    
+
+########
+# Checks an userInput string against stringToCheck
+# Returns newString, commandFound
+def commandCheck(userInput, stringToCheck, length):
+  newString = ''
+  commandFoundBool = false
+  correctCharCountIndex = 0
+  userInputStringLength = len(userInput)
+  #print len(userInput)
+  for i in range (0, len(stringToCheck)):
+    if ( correctCharCountIndex == length ):
+      # Whole string checked
+      print newString
+      return newString, commandFoundBool
+    elif (userInput[correctCharCountIndex] == stringToCheck[i]):
+      # This character matches the string
+      newString = newString + userInput[correctCharCountIndex]
+      correctCharCount = correctCharCountIndex + 1
+      print len(newString)
+      print length
+      if (len(newString) <= length):
+        # Matched
+        commandFoundBool = true
+        print newString
+        return newString, commandFoundBool
+    else:
+      correctCharCountIndex = 0
+      newString = ''
+      #print 'none new:'+newString + ' state:'+str(commandFoundBool)
+
+def printPadding():
+  print '\n\n'
+
+#Call to start game
+main()
